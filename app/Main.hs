7d069f8d523e24f8c2141bb6ded95eb3130be76d
Liam Griffin <gryffskin@gmail.com>
2019-03-27 16:56:38 +0000

    Get java code from files in a directory

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,20 @@
 module Main where
 
+import System.Directory
+import Control.Concurrent.ParallelIO.Global
 import Language.Java.Parser
 
 import Lib
 
 main :: IO ()
 main = do
-  let plantUml = generatePlantUml $ parser compilationUnit "public class MyClass {}; public class AnotherClass {}"
+  javaFileNames <- listDirectory "javaTestFiles"
+  let javaFilePaths = fmap ("javaTestFiles/" ++) javaFileNames
+  javaFileContents <- parallel (fmap readFile javaFilePaths)
+  let javaCode = unlines javaFileContents
+  let ast = parser compilationUnit javaCode
+  let plantUml = generatePlantUml ast
   case plantUml of (Left _) -> putStrLn "failed to parse the java code"
                    (Right output) -> writeFile "output.plantuml" output >> putStrLn "plantuml generated at output.plantuml"
+  stopGlobalPool
 
